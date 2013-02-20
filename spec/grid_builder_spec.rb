566123ddb616c6161b97a7913fb83adf3fe3730e
Amiel Martin <amiel.martin@gmail.com>
2013-02-19 19:37:47 -0800

    Use inspect to verify builder

--- spec/grid_builder_spec.rb
+++ spec/grid_builder_spec.rb
@@ -1,17 +1,17 @@
 require 'grid_builder'
 
 describe GridBuilder do
-  it 'splits up a file' do
+  it 'parses a stream' do
     builder = GridBuilder.new StringIO.new <<-GRID
 |---|
 | | |
 |---|
 GRID
 
-    builder.grid.matrix.collect(&:class).should == Matrix[
-      [Wall, Wall, Wall, Wall, Wall],
-      [Wall, Floor, Wall, Floor, Wall],
-      [Wall, Wall, Wall, Wall, Wall],
+    builder.grid.matrix.collect(&:inspect).should == Matrix[
+      ['Wall()', 'Wall()', 'Wall()', 'Wall()', 'Wall()'],
+      ['Wall()', 'Floor()', 'Wall()', 'Floor()', 'Wall()'],
+      ['Wall()', 'Wall()', 'Wall()', 'Wall()', 'Wall()'],
     ]
   end
 end
